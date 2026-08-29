5f87703a1c87b70647f7034bd620d1db32c21c3e
antngu <antngu@gmail.com>
2026-08-29 12:23:04 +0800

    Fix hidden mobile navigation during onboarding

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bahasa-bintang-v3';
+const CACHE_NAME = 'bahasa-bintang-v4';
 const APP_FILES = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
